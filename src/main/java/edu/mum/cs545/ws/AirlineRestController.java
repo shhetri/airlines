dcb331b9d7f5f494efee0c2666d6812000facd43
Sumit Chhetri <er.sumit.chhetri@gmail.com>
2017-06-09 00:41:42 -0500

    add method to view a particular airline

--- src/main/java/edu/mum/cs545/ws/AirlineRestController.java
+++ src/main/java/edu/mum/cs545/ws/AirlineRestController.java
@@ -20,6 +20,21 @@ public class AirlineRestController {
         return airlineService.findAll();
     }
 
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("{id}")
+    public Response find(@PathParam("id") int id) {
+        Airline airline = new Airline();
+        airline.setId(id);
+        airline = airlineService.find(airline);
+
+        if (airline == null) {
+            return Response.status(Response.Status.NOT_FOUND).entity(String.format("Airline with id %d doesn't exist.", id)).build();
+        }
+
+        return Response.ok(airline).build();
+    }
+
     @Path("create")
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
